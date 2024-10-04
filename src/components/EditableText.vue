5cbf3dd5198d616fa1b9e11eb77079dc1dcfdf6f
AnupamKris <anupamkris13262@gmail.com>
2024-10-05 00:03:35 +0530

    ui revamp

---
+++ src/components/EditableText.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <p v-if="!editing" @click="edit">{{ value }}</p>
+    <input
+      v-else
+      type="text"
+      v-model="value"
+      ref="inputRef"
+      class="outline-none bg-transparent"
+      @blur="editing = false"
+      @keydown.enter="editing = false"
+    />
+  </div>
+</template>
+
+<script setup lang="js">
+// const props = defineProps({
+//   text: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+const value = defineModel();
+const editing = ref(false);
+const inputRef = ref(null);
+const edit = () => {
+  editing.value = true;
+  nextTick(() => {
+    inputRef.value.focus();
+  });
+};
+</script>
+
+<style scoped></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,32 +1,94 @@
 <template>
-  <div class="copy p-0 flex">
-    <div
-      v-for="(container, key) in containers"
-      :key="key"
-      class="item h-screen max-w-[100px]"
-    >
-      <h3>{{ container.name }}</h3>
-      <Container
-        :class="{ 'source-container': key === 'items1' }"
-        class="bg-gray-200 rounded-md p-2 h-full"
-        :behaviour="key === 'items1' ? 'copy' : undefined"
-        group-name="1"
-        :get-child-payload="(index) => getChildPayload(key, index)"
-        @drop="(e) => onDrop(key, e)"
+  <div class="copy p-0 flex bg-gray-200">
+    <div class="w-[70px]"></div>
+    <div class="flex flex-col w-full">
+      <div class="h-[7vh] w-full"></div>
+      <div
+        class="flex flex-col bg-slate-50 w-full rounded-xl overflow-hidden shadow-xl p-5 h-full gap-5"
       >
-        <Draggable
-          v-for="item in container.items"
-          :key="item.id"
-          class="min-h-[160px] p-2 flex justify-center items-center"
-        >
-          <div class="draggable-item">
-            {{ item.data }}
+        <div class="flex gap-3 items-center">
+          <h1 class="text-xl font-semibold py-3">My Tasks</h1>
+          <button
+            class="flex items-center gap-1 bg-indigo-700 h-fit py-2 px-3 text-slate-50 rounded-md shadow-xl"
+            @click="addItem"
+          >
+            <PlusCircle class="w-4 h-4" />
+            <p class="text-xs">Add Task</p>
+          </button>
+          <button
+            class="flex items-center gap-1 bg-indigo-700 h-fit py-2 px-3 text-slate-50 rounded-md shadow-xl"
+            @click="addColumn"
+          >
+            <PlusCircle class="w-4 h-4" />
+            <p class="text-xs">Add Column</p>
+          </button>
+        </div>
+        <div class="flex bg-slate-50 w-full h-full gap-5">
+          <div
+            v-for="(container, key) in containers"
+            :key="key"
+            class="item h-full min-h-full"
+          >
+            <h3 class="text-sm text-slate-500">
+              <EditableText v-model="container.name" />
+            </h3>
+            <Container
+              class="bg-slate-100 rounded-lg px-4 py-2 h-full"
+              :behaviour="key === 'items1' ? 'move' : undefined"
+              group-name="1"
+              :get-child-payload="(index) => getChildPayload(key, index)"
+              @drop="(e) => onDrop(key, e)"
+            >
+              <Draggable
+                v-for="(item, index) in container.items"
+                :key="item.id"
+                class="p-6 flex justify-center bg-white rounded-xl flex-col mt-2"
+                :class="
+                  (index !== 0 ? '!mt-4 ' : ' ') +
+                  key.toLowerCase().split(' ').join('')
+                "
+              >
+                <span
+                  class="bg-indigo-200 text-indigo-800 text-xs rounded-full py-1.5 px-4 w-fit"
+                >
+                  UI Design
+                </span>
+                <div class="text-xl mt-4">
+                  {{ item.data }}
+                </div>
+                <div class="flex items-center gap-3">
+                  <div class="progress w-full h-1 bg-gray-200 rounded-full">
+                    <div
+                      class="bg-indigo-500 h-full rounded-full"
+                      :style="{ width: `${73}%` }"
+                    ></div>
+                  </div>
+                  <p class="flex items-center text-xs">73%</p>
+                </div>
+                <div class="flex items-center justify-between gap-3 mt-4">
+                  <div class="flex items-center gap-2">
+                    <Clock class="w-4 h-4 text-slate-400" />
+                    <p class="text-xs text-slate-400">Due : Feb 03, 2024</p>
+                  </div>
+                  <div class="flex">
+                    <span
+                      class="bg-indigo-200 text-indigo-800 border-2 border-white text-[8px] rounded-full min-h-6 min-w-6 flex items-center justify-center scale-[1.25]"
+                    >
+                      AK
+                    </span>
+                    <span
+                      class="bg-green-200 text-green-800 border-2 border-white text-[8px] rounded-full min-h-6 min-w-6 flex items-center justify-center scale-[1.25]"
+                    >
+                      AK
+                    </span>
+                  </div>
+                </div>
+              </Draggable>
+            </Container>
           </div>
-        </Draggable>
-      </Container>
-      <button @click="() => addItem(key)" class="add-item-btn">Add Item</button>
+        </div>
+      </div>
     </div>
-    <button @click="addColumn" class="add-column-btn">Add Column</button>
   </div>
 </template>
 
@@ -34,6 +96,8 @@
 import { ref } from "vue";
 import { Container, Draggable } from "vue-dndrop";
 import { applyDrag, generateItems } from "@/utils/helpers";
+import { Clock } from "lucide-vue-next";
+import { PlusCircle } from "lucide-vue-next";
 
 const containers = ref({
   items1: {
@@ -51,12 +115,10 @@ const containers = ref({
 });
 
 const onDrop = (key, dropResult) => {
-  if (key !== "items1") {
-    containers.value[key].items = applyDrag(
-      containers.value[key].items,
-      dropResult
-    );
-  }
+  containers.value[key].items = applyDrag(
+    containers.value[key].items,
+    dropResult
+  );
 };
 
 const getChildPayload = (key, index) => containers.value[key].items[index];
@@ -65,14 +127,12 @@ const addColumn = () => {
   const newKey = `items${Object.keys(containers.value).length + 1}`;
   containers.value[newKey] = {
     name: `Column ${Object.keys(containers.value).length + 1}`,
-    items: generateItems(15, (i) => ({
-      id: `${newKey}${i}`,
-      data: `Draggable ${newKey} - ${i}`,
-    })),
+    items: [],
   };
 };
 
-const addItem = (key) => {
+const addItem = () => {
+  const key = Object.keys(containers.value)[0];
   const newIndex = containers.value[key].items.length;
   containers.value[key].items.push({
     id: `${key}${newIndex}`,
@@ -119,5 +179,15 @@ h3 {
   width: 100%;
   height: 100%;
   min-height: 160px;
+}
+
+.items1 {
+  @apply border-l-4 border-red-300;
+}
+.items2 {
+  @apply border-l-4 border-blue-300;
+}
+.items3 {
+  @apply border-l-4 border-green-300;
 }
 </style>
